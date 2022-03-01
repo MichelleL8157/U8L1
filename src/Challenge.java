3ef2c14ec07dfea430fbd5ee39d3c6de367730c0
BT_1E10_10 <eleanorc25@nycstudents.net>
2022-03-01 12:33:05 -0500

    Initial commit

---
+++ src/Challenge.java
@@ -0,0 +1,17 @@
+public class Challenge {
+    public static void main(String[] args) {
+        String[][] originalSeats = {{"Moiz", "1"}, {"Angie", "2"}, {"Taran", "3"},
+                {"Kelvin", "4"}, {"Kyler", "5"}, {"David", "6"},
+                {"WenHao", "7"}, {"Nicole", "8"}, {"Jennifer", "9"},
+                {"Michelle", "10"}, {"Beckett", "11"}, {"Raymond", "12"},
+                {"Lucy", "13"}, {"Apramjot", "14"}, {"Justin Lema", "15"},
+                {"Sam", "16"}, {"Tristan", "17"}, {"Pradeep", "18"},
+                {"Mohammad", "19"}, {"Haley", "20"}, {"n/a", "21"},
+                {"Rely", "22"}, {"Cheng Han", "23"}, {"Qihan", "24"},
+                {"Kevin", "25"}, {"Ryan", "26"}, {"Justin Lin", "27"},
+                {"Jeffrey", "28"}, {"Danny", "29"}, {"Elliot", "30"},
+                {"Benson", "31"}, {"Fiona", "32"}, {"Neil", "33"}, {"Kaitlyn", "33"}};
+
+
+    }
+}
