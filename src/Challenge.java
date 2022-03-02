7fa4d1a75414614c40ef9dc40407a771d947cb71
Michelle Liu <michellel246@nycstudents.net>
2022-03-02 01:56:16 -0500

    implemented code to produce new seats. need to fix code to meet requirements of proj.

--- src/Challenge.java
+++ src/Challenge.java
@@ -1,17 +1,69 @@
+import java.util.Arrays;
+
 public class Challenge {
     public static void main(String[] args) {
-        String[][] originalSeats = {{"Moiz", "1"}, {"Angie", "2"}, {"Taran", "3"},
-                {"Kelvin", "4"}, {"Kyler", "5"}, {"David", "6"},
-                {"WenHao", "7"}, {"Nicole", "8"}, {"Jennifer", "9"},
-                {"Michelle", "10"}, {"Beckett", "11"}, {"Raymond", "12"},
-                {"Lucy", "13"}, {"Apramjot", "14"}, {"Justin Lema", "15"},
-                {"Sam", "16"}, {"Tristan", "17"}, {"Pradeep", "18"},
-                {"Mohammad", "19"}, {"Haley", "20"}, {"n/a", "21"},
-                {"Rely", "22"}, {"Cheng Han", "23"}, {"Qihan", "24"},
-                {"Kevin", "25"}, {"Ryan", "26"}, {"Justin Lin", "27"},
-                {"Jeffrey", "28"}, {"Danny", "29"}, {"Elliot", "30"},
-                {"Benson", "31"}, {"Fiona", "32"}, {"Neil", "33"}, {"Kaitlyn", "33"}};
-
-
+        String[][] originalSeats = {{"Moiz", "Angie", "Taran", "Kelvin", "Kyler", "David", "WenHao Huang", "Nicole", "Jennifer", "Michelle", null, null},
+                {"Beckett", "Raymond", "Lucy", "Apramjot", "Justin Lema", "Sam", "Tristan", "Pradeep", "Mohammad", "Haley", null, "Rely"},
+                {"Cheng Han", ",Qihan", "Kevin", "Ryan", "Justin Liu", "Jeffrey", "Danny", "Elliot", "Benson", "Fiona", "Neil", "Kaitlyn"}};
+        String[][] newSeats = new String[originalSeats.length][originalSeats[0].length];
+        boolean redo = false;
+        for (int row = 0; row != originalSeats.length; row++) {
+            for (int seat = 0; seat != originalSeats[0].length; seat++) {
+                if (originalSeats[row][seat] != null) {
+                    //System.out.println("Testing: " + originalSeats[row][seat]);
+                    int originalSeat = seat + (row * 11); //                                                             0
+                    int randSeat = (int) (Math.random() * 12) + ((int) (Math.random() * 3)  * 11);//                     12
+                    while (originalSeat == randSeat) { //makes sure original seat is not the same as new seat
+                        randSeat = (int) (Math.random() * 12) + ((int) (Math.random() * 3)  * 11);
+                    }
+                    int newSeat = randSeat;//                                                                            12
+                    int newRow = 0;
+                    while (newSeat > 11) {
+                        newSeat = newSeat - 11;
+                        newRow++;
+                    }
+                    if (newSeats[newRow][newSeat] != null) { //makes sure the new seat is empty
+                        randSeat = (int) (Math.random() * 12) + ((int) (Math.random() * 3)  * 11);
+                        while (originalSeat == randSeat) { //makes sure original seat is not the same as new seat
+                            randSeat = (int) (Math.random() * 12) + (row * 11);
+                        }
+                        newSeat = randSeat;
+                        newRow = 0;
+                        while (newSeat > 11) {
+                            newSeat = newSeat - 11;
+                            newRow++;
+                        }
+                    }
+                    newSeats[newRow][newSeat] = originalSeats[row][seat];
+                }
+            }
+            for (int check = 0; check != newSeats[0].length; check++) {
+                if (check == 0) { //check if left of 1st person is empty
+                    if (newSeats[row][1] == null) {
+                        redo = true;
+                    }
+                }
+                else if (check == newSeats[0].length - 1) { //check if right of last person is empty
+                    if (newSeats[row][newSeats[0].length - 2] == null) {
+                        redo = true;
+                    }
+                }
+                else {
+                    if (newSeats[row][check + 1] == null && newSeats[row][check - 1] == null) {
+                        redo = true;
+                    }
+                }
+            }
+            if (!redo) {
+                System.out.println(Arrays.toString(newSeats[row]));
+            }
+            else {
+                for (int clear = 0; clear != newSeats[0].length; clear++) {
+                    newSeats[row][clear] = null;
+                }
+                row--;
+                redo = false;
+            }
+        }
     }
 }
